028b0f6c3458db672310188b39f15d7136bb0a16
christopherinvans <106615358+christopherinvans@users.noreply.github.com>
2022-09-22 13:57:44 -0700

    installed morgan logging middleware for express

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
+
+app.use(morgan('common'));
 
 let topMovies = [
     {
@@ -17,7 +20,8 @@ let topMovies = [
 ];
 
 app.get('/', (req, res) => {
-    res.send('Abandon Hope All Ye Who Enter Here!');
+    res.send('Abandon Hope All Ye Who Enter Here!'); 
+    // UPDATE TO SERIOUS MESSAGE LATER
 });
 
 app.get('/documentation', (req, res) => {                  
