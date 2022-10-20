7d3215174ccd930bd3963728e2afdbd24974d20a
christopherinvans <106615358+christopherinvans@users.noreply.github.com>
2022-10-20 09:23:40 -0700

    installed cors

--- index.js
+++ index.js
@@ -19,6 +19,9 @@ const express = require('express');
     app.use(bodyParser.json());
     // app.use(bodyParser.urlencoded({ extended: true })); from curriculum
 
+    const cors = require('cors');
+    app.use(cors());
+
     let auth = require('./auth')(app);
 
     const passport = require('passport');
