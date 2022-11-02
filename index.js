99df05c2763a9f21cdd521a4beeacf5b9023d7af
UpdatedUserName <106615358+christopherinvans@users.noreply.github.com>
2022-11-02 13:54:01 -0700

    temporarily removed authentication

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ app.get('/documentation', (req, res) => {
 });
 
 // SHOW ALL MOVIES
-app.get('/movies', passport.authenticate('jwt', {session: false}), (req, res) => {
+app.get('/movies', (req, res) => {
     Movies.find()
       .then((movies) => {
         res.status(201).json(movies);
